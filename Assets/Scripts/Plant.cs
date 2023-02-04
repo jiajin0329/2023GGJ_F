446c51f9ca7bf4ba4a477e762f46dc2c51efa9e3
as1942565 <as1942565@gmail.com>
2023-02-04 17:30:16 +0800

    基本雛型完工

---
+++ Assets/Scripts/Plant.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour
+{
+    public BoxCollider2D PlantColloder;
+    public BoxCollider2D RootColloder;
+}
