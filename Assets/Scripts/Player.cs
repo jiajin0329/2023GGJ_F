326a6a41f142f8645ac0fd9fcffa69af4bccb503
dsacxz0 <124366371+dsacxz0@users.noreply.github.com>
2023-02-05 15:24:21 +0800

    Fixed Cloud Position, Added link to Github

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -28,6 +28,7 @@ public class Player : MonoBehaviour
     {
         Move ();
         Camera ();
+        cloud.transform.position = new Vector3(Cam.transform.position.x, cloud.transform.position.y, cloud.transform.position.z);
         if (transform.position.y <= -5f && dead == false)
         {
             Die();
---
+++ Assets/Scripts/Recenter.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recenter : MonoBehaviour
+{
+    public void ResetPosistion()
+    {
+        transform.position = Vector3.zero;
+    }
+}
